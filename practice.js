c765ee1cc250a70bde00da5484db4be98b6c252a
Rahul21-art <naidurahul2105@gmail.com>
2025-09-06 12:33:49 +0530

    learnt

--- practice.js
+++ practice.js
@@ -1,1 +1,37 @@
 console.log("Hello, World!,rahul");
+
+let name="rahul";
+console.log(name);
+
+let age=21;
+console.log(age);
+
+let islogged=false;
+console.log(islogged);
+
+let x;
+console.log(x);
+
+let y=null;
+console.log(y);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof islogged);
+console.log(typeof x);
+console.log(typeof y);
+
+let arr =[1,2,3,4,5];
+console.log(arr);
+console.log(typeof arr);
+
+let person = {
+    name:"rahul",
+    age:21,
+    islogged:false,
+}
+console.log(person);
+console.log(typeof person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.islogged);
