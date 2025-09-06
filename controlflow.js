be7273960b05ba767e1fa084d55025b0a20e6a4b
Rahul21-art <naidurahul2105@gmail.com>
2025-09-06 15:36:38 +0530

    loopsf

---
+++ controlflow.js
@@ -0,0 +1,51 @@
+let day=5
+
+switch(day){
+    case 1:console.log("monday");
+    break;
+    case 2:console.log("tuesday");
+    break;
+    case 3:console.log("wednesday");
+    break;
+    case 4:console.log("thursday");
+    break;
+    case 5:console.log("friday");
+    break;
+    case 6:console.log("saturday");
+    break;
+    case 7:console.log("sunday");
+    break;
+    default:console.log("invalid day");
+}
+
+for(j=1;j<=10;j++)
+{
+    console.log(j)
+}
+
+let i=1
+while (i<2)
+{ 
+    for(i=1;i<=10;i++)
+    console.log(i);
+    i++;
+}
+
+let person ={
+    name:"rahul",
+    age:20,
+    yfgukygP:684584
+
+}
+for(let key in person){
+    console.log(`${key}: ${person[key]}`)
+}
+
+i=1
+while(i<=10){
+    if (i===5){git 
+        break;
+    }
+    console.log(i);
+    i++;
+}
